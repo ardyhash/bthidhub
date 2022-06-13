c18d7d902268d8f6e7ee403226145135f2a58fa8
ardyhash <ardyhash@gmail.com>
2022-06-13 19:30:39 -0400

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -1,11 +1,12 @@
 echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null
 
-cd ~
+workingdir=/usr/local/src
+cd $workingdir
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install git -y
 
-git clone https://github.com/ruundii/bthidhub
-cd ~/bthidhub/install/on_rpi
+git clone https://github.com/ardyhash/bthidhub
+cd bthidhub/install/on_rpi
 sh ./on_pi_setup.sh
